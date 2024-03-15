65e4745f1d6d7623c7908044d285bdd5b2520eef
AmanSingh494 <amanforwork1@gmail.com>
2024-03-15 10:42:42 +0530

    changed template

--- server.js
+++ server.js
@@ -11,47 +11,44 @@ app.use(cors({ origin: 'http://localhost:3000' }))
 // post request
 app.post('/submit', async (req, res) => {
   try {
-    const { name, email, phone, address, about } = req.body
-    console.log(name, email, phone, address, about)
+    const {
+      name,
+      email,
+      phone,
+      address,
+      about,
+      achievements,
+      edu,
+      skill,
+      work,
+      links
+    } = req.body
+    console.log(
+      name,
+      email,
+      phone,
+      address,
+      about,
+      achievements,
+      edu,
+      skill,
+      work,
+      links
+    )
     // html template
     const template = createTemplate('index', {
-      name: 'Aman Singh',
-      email: 'amanforwork1@gmail.com',
-      phone: 9717599542,
-      contactLinks: [
-        'www.goggle.com',
-        'www.pinterest.com',
-        'www.goggle.com',
-        'www.pinterest.com'
-      ],
-      qualifications: ['B.sc hons cs', 'B.tech Cs', 'Phd cs'],
-      education: [
-        'Ch. Chhabil Daas Public School',
-        'Keshav Mahavidyalaya',
-        'Delhi University'
-      ],
-      work: [
-        {
-          position: 'Software Developer',
-          company: 'XYZ Company',
-          years: '2018 - Present'
-        },
-        {
-          position: 'Junior Developer',
-          company: 'ABC Company',
-          years: '2016 - 2018'
-        }
-      ],
-      about: `I'm a passionate and dedicated software developer with over 4 years of professional experience. I specialize in building high-quality, scalable web applications and have a deep understanding of JavaScript and its frameworks. I'm always eager to learn new technologies and improve my skills. I believe in writing clean, efficient code and I'm experienced in working with agile methodologies. I'm a strong team player, but I'm also capable of working independently. I'm highly motivated, I love problem-solving and my ultimate goal is to make a significant impact through my work.`,
-      skills: [
-        'HTML',
-        'CSS',
-        'JavaScript',
-        'React',
-        'Node.js'
-        // Add more skills as needed
-      ]
+      name,
+      email,
+      phone,
+      links,
+      achievements,
+      edu,
+      work,
+      address,
+      about,
+      skill
     })
+    console.log(template)
     const pdfBuffer = await htmlToPdf(template)
     res.contentType('application/pdf')
     res.setHeader('Content-Disposition', 'attachment; filename=resume.pdf')
